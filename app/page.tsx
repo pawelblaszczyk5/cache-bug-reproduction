f7ed4d456bd8da8ea1de36afd6fc64bc30d1e554
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-16 18:07:16 +0200

    Force dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { exampleAction } from "@/app/action";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <main>
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default async function TestPage() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return <h1>Hello world</h1>;
